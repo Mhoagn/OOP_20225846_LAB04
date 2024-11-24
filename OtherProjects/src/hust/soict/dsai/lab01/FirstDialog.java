b4d84c95483a15a6cc26a70c1102e20582742122
MinhHoang <minhhoangdayyyy07@gmail.com>
2024-11-24 16:44:41 +0700

    add other project

---
+++ OtherProjects/src/hust/soict/dsai/lab01/FirstDialog.java
@@ -0,0 +1,8 @@
+package hust.soict.dsai.lab01;
+import javax.swing.JOptionPane;
+// add new
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World");
+    }
+}
